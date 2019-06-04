9506a4f7b7ccf445d74bdb0aef1cd178e8237ae4
Alex100399 <YOU@EXAMPLE.COM>
2019-06-04 22:06:30 +0300

    Балашов Форма авторизации

---
+++ WindowsFormsAuthorization2/WindowsFormsAuthorization/WindowsFormsAuthorization/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsAuthorization
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0) // проверяем введён ли логин
+            {
+                if (textBox2.Text.Length > 0) // проверяем введён ли пароль
+                { 
+                    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\DatabaseAuthorization.mdf;Integrated Security=True");
+                    con.Open();
+            string query = "SELECT * FROM [UserAuthoriz] WHERE [Логин]= '" + textBox1.Text + "' AND [Пароль] = '" + textBox2.Text + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con); //("SELECT * FROM user WHERE [Пользователи]= '"+ textBox1.Text +"' and [Пароль] = '" + textBox2.Text + "'", con);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+                    if (dtbl.Rows.Count > 0) // если такая запись существует
+                    {
+                        MessageBox.Show("Пользователь авторизовался"); // говорим, что авторизовался
+                        this.Hide();
+                        Form2 f = new Form2();
+                        f.Show();
+
+                    }
+                    else MessageBox.Show("Пользователя не найден"); // выводим ошибку
+                }
+                else MessageBox.Show("Введите пароль"); // выводим ошибку
+            }
+            else MessageBox.Show("Введите логин"); // выводим ошибку
+        }
+           
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form1 f1 = new Form1();
+            Form t = new Form3(sqlConnection);
+            f1.Close();
+            t.Show();
+            
+   
+        }
+    } 
+}
---
+++ WindowsFormsAuthorization2/WindowsFormsAuthorization/WindowsFormsAuthorization/Form3.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Configuration;
+
+
+
+namespace WindowsFormsAuthorization
+{
+
+    public partial class Form3 : Form
+    {
+        private SqlConnection sqlConnection = null;
+
+        
+        public Form3(SqlConnection con )
+        {
+            InitializeComponent();
+
+            sqlConnection = con;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (textBox1.Text.Length > 0) // проверяем логин
+            {
+
+            }
+            else MessageBox.Show("Укажите логин");
+
+                if (textBox2.Text.Length > 0) // проверяем пароль
+                {
+
+                }
+                else MessageBox.Show("Укажите пароль");
+            
+                
+                    if (textBox4.Text.Length > 0) // проверяем второй пароль
+                    {
+
+                    }
+                    else MessageBox.Show("Повторите пароль");
+
+            //if (textBox2.TextLength > 6)
+            //{
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Длина пароля меньше допустимой. Минимальная длина 6 символа.");
+            //}
+          
+
+
+            if (textBox2.TextLength < 32)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Длина пароля превышает допустимую. Максимальная длина 32 символов.");
+            }
+
+            if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Одно из полей не заполнено");
+            }
+            else
+            if (textBox2.TextLength > 6)
+            {
+                if (textBox2.TextLength < 32)
+                {
+                    if (textBox2.Text == textBox4.Text) // проверка на совпадение паролей
+                    {
+                        MessageBox.Show("Пользователь зарегистрирован");
+                    }
+
+                    else MessageBox.Show("Пароли не совподают");
+                }
+               else MessageBox.Show("Длина пароля превышает допустимую. Максимальная длина 32 символов.");
+            }
+            else MessageBox.Show("Длина пароля меньше допустимой. Минимальная длина 6 символа.");
+
+
+
+
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-IJHI4LO\SQLEXPRESS;Initial Catalog=AUTHORIZATION;Integrated Security=True");
+            string sql = "INSERT INTO [UserAuthoriz] (Логин, Пароль, [Имя_пользователя]) VALUES (@Логин, @Пароль, @Имя_пользователя)";
+            SqlCommand insertUserNameCommand = new SqlCommand(sql, con);
+            insertUserNameCommand.Parameters.AddWithValue("Логин", textBox1.Text);
+                insertUserNameCommand.Parameters.AddWithValue("Пароль", textBox2.Text);
+                insertUserNameCommand.Parameters.AddWithValue("Имя_пользователя", textBox3.Text);
+
+            try
+            {
+                con.Open();
+                int n = insertUserNameCommand.ExecuteNonQuery();
+               
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+            }
+            this.Hide();
+            Form1 y = new Form1();
+            y.Show();
+
+            //try
+            //{
+            //    await insertUserNameCommand.ExecuteNonQueryAsync();
+
+            //    Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+        }
+
+        
+    }
+}
